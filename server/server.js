1c3579142a6ab1666219a8763c55cda7e01af945
Iyyappan P.A <iyyappan98@example.com>
2025-11-19 12:29:34 +0530

    Fix CORS: allow Netlify origin and enable preflight

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', 'https://iyyappan-mern-auth.netlify.app']
+const allowedOrigins = ['http://localhost:5173', 'https://iyyappan-mern-auth.netlify.app/']
 
 const corsOptions = {
   origin: function(origin, callback) {
